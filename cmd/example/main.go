c7e54ac82e5afc1902d7c130a1566c0740b1932b
sashaholovenko <55443197+sashaholovenko@users.noreply.github.com>
2021-11-07 14:48:55 +0200

    Update main.go
    fixed some problems with variables

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,46 +1,47 @@
 package main
 
 import (
-	"io"
-	"os"
-	"flag"
-	"fmt"
-	"strings"
-	Lab2 "https://github.com/IP94-rocketBunny-architecture/Lab2"
+  "flag"
+  "fmt"
+  "io"
+  "os"
+  "strings"
+
+  Lab2 "github.com/IP94-rocketBunny-architecture/Lab2"
 )
 
-func getFlagsValues() (inputExpression, fileIn, fileout *string) {
-	defer flag.Parse()
-	
-	inputExpression = flag.String("e", "", "Expression to compute")
-	filein = flag.String("i", "", "input file")
-	fileout = flag.String("o", "", "output file")
-	return
+func getFlagsValues() (inputExpression, fileIn, fileOut *string) {
+  defer flag.Parse()
+
+  inputExpression = flag.String("e", "", "Expression to compute")
+  fileIn = flag.String("i", "", "input file")
+  fileOut = flag.String("o", "", "output file")
+  return
 }
 
 func main() {
-	var in io.Reader
-	var out io.Writer
-	var inputExpression, fileIn, fileout *string = getFlagsValues()
-	
-	if *fileIn != "" && *inputExpression != "" {
-		err := fmt.Errorf("more than one expr is not needed")
-		panic(err)
-	}
-	
-	if *inputExpression != "" {
-		in = strings.NewReader(*inputExpression)
-	} else if *fileIn != "" {
-		in, _ = os.Open(*fileIn)
-	}
-	if *fileout != "" {
-		out, _ = os.Create(*outputFilename)
-	} else {
-		out = os.Stdout
-	}
-	handler := Lab2.ComputeHandler{Input: in, Output: out}
-	err := handler.Compute()
-	if err != nil {
-		panic(err)
-	}
+  var in io.Reader
+  var out io.Writer
+  var inputExpression, fileIn, fileout *string = getFlagsValues()
+
+  if *fileIn != "" && *inputExpression != "" {
+    err := fmt.Errorf("more than one expr is not needed")
+    panic(err)
+  }
+
+  if *inputExpression != "" {
+    in = strings.NewReader(*inputExpression)
+  } else if *fileIn != "" {
+    in, _ = os.Open(*fileIn)
+  }
+  if *fileout != "" {
+    out, _ = os.Create(*fileout)
+  } else {
+    out = os.Stdout
+  }
+  handler := Lab2.ComputeHandler{Input: in, Output: out}
+  err := handler.Compute()
+  if err != nil {
+    panic(err)
+  }
 }
